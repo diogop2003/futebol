318bfea0bc788eafa45c98ed52dcbe74fd25f010
diogo <diogoprado1511@gmail.com>
2024-11-09 00:17:12 -0300

    feat churrasco page

---
+++ pelada-front/src/app/pages/churrasco/churrasco.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChurrascoComponent } from './churrasco.component';
+
+describe('ChurrascoComponent', () => {
+  let component: ChurrascoComponent;
+  let fixture: ComponentFixture<ChurrascoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChurrascoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChurrascoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ pelada-front/src/app/pages/churrasco/churrasco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-churrasco',
+  standalone: true,
+  imports: [],
+  templateUrl: './churrasco.component.html',
+  styleUrl: './churrasco.component.css'
+})
+export class ChurrascoComponent {
+
+}
