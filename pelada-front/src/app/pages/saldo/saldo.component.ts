d40b7eab6f99122992af7529d2f13d52d0747175
diogo <diogoprado1511@gmail.com>
2024-11-09 00:18:01 -0300

    feat saldo pages

---
+++ pelada-front/src/app/pages/saldo/saldo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saldo',
+  standalone: true,
+  imports: [],
+  templateUrl: './saldo.component.html',
+  styleUrl: './saldo.component.css'
+})
+export class SaldoComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
