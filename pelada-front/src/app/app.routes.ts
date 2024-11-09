7b25f7de9036fdf4d09f85606c5e75fe75d819b9
diogo <diogoprado1511@gmail.com>
2024-11-09 00:16:52 -0300

    fix: att rotas

--- pelada-front/src/app/app.routes.ts
+++ pelada-front/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { SaldoComponent } from './pages/saldo/saldo.component';
+import { DiaristasComponent } from './pages/diaristas/diaristas.component';
+import { MensalistaComponent } from './pages/mensalista/mensalista.component';
+import { ChurrascoComponent } from './pages/churrasco/churrasco.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: SaldoComponent },
+  { path: 'diaristas', component: DiaristasComponent },
+  { path: 'mensalistas', component: MensalistaComponent },
+  { path: 'churrasco', component: ChurrascoComponent },
 ];
