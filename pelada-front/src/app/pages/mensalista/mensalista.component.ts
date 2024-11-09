399207e1d230774034d483d89f6fb8695de9b7ca
diogo <diogoprado1511@gmail.com>
2024-11-09 00:17:49 -0300

    feat mensalistas page

---
+++ pelada-front/src/app/pages/mensalista/mensalista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensalista',
+  standalone: true,
+  imports: [],
+  templateUrl: './mensalista.component.html',
+  styleUrl: './mensalista.component.css'
+})
+export class MensalistaComponent {
+
+}
